08dcfdadd23815cdd522ea0e68b8d4d836eac3f2
Benjamin <fireblazegaming21@gmail.com>
2025-03-17 00:34:15 +0100

    Added some responsivnes for phones
    TODO:
    - Fix link to github -> make <a> take up the entire tag
    - Fix responsivness of a pc page (not that important)
    - Fix resposnivness for smallest phones
    - Fix slider buttons on phones
    - Fix themes
    - Finish menus
    - Add logic to the buttons
    
    Co-Authored-By: BinCho06 <141220263+BinCho06@users.noreply.github.com>

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,13 @@ const dropDownTitleSpan = dropDownButton.children[0];
 const lightDarkModeSwitch = document.querySelector('.light-dark-toggle');
 const dropdownToggle = document.querySelector('.options-toggle-container').children[0];
 const githubImageContainer = document.querySelector('.github-icon-holder');
+const githubDark = `<svg width="98" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>`;
+const githubLight = `<svg width="98" height="96" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/></svg>`;
 const nodeImage = document.querySelector('.node-logo');
 const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
-
+const html = document.querySelector('html');
+const chart = document.querySelector('#chart');
 let selectedOption = '';
 
 console.log(menuBtns);
@@ -32,9 +35,12 @@ lightDarkModeSwitch.addEventListener("click", ()=>{
 
     if(body.classList.contains('darkmode')){
         nodeImage.setAttribute('src','icons/nodejsLight.svg');
-    }else
-        nodeImage.setAttribute('src','icons/nodejsDark.svg')
+        githubImageContainer.innerHTML = githubLight;
 
+    }else{
+        nodeImage.setAttribute('src','icons/nodejsDark.svg');
+        githubImageContainer.innerHTML = githubDark;
+    }
 });
 
 dropdownToggle.addEventListener("click", (e) =>{
@@ -141,9 +147,9 @@ function limitDate(input){
     }
     validate();
 }
-
 window.onload = () =>{
     limitDate(startEndInp[0]);
+    isMobile();
 }
 
 function validate(){
@@ -175,3 +181,17 @@ function moveSlider(index){
 }
 
 // TODO: Naredi graph switch verification
+
+function isMobile() {
+    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    console.log("User is on a phone!");
+    body.classList.toggle('phone');
+    html.classList.toggle('phone');
+    chart.setAttribute('width',350)
+    chart.setAttribute('height', 400);
+} else {
+    console.log("User is on a desktop or tablet.");
+}
